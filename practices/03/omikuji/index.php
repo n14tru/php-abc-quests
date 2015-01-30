dd5af07cd16825650d438bb445406260e7ecc13f
n14.tru <n14.tru@gmail.com>
2015-01-30 20:16:01 +0900

    おみじくプログラム　応用編

--- practices/03/omikuji/index.php
+++ practices/03/omikuji/index.php
@@ -3,18 +3,18 @@ switch (rand(0,9)) {
 	case 0;
 	case 1;
 	case 2;
-		$result = '大凶';
+		$result = 'ごめんなさい。あなたの今日の運勢は<b>大凶</b>です。';
 		break;
 	case 3;
 	case 4;
 	case 5;
 	case 6;
 	case 7;
-		$result = '吉';
+		$result = 'あなたの今日の運勢は<b>吉</b>です。';
 		break;
 	case 8;
 	case 9;
-		$result = '大吉';
+		$result = 'おめでとうございます！あなたの今日の運勢は<b>大吉</b>です。';
 		break;
 }
 ?>
@@ -25,6 +25,6 @@ switch (rand(0,9)) {
 	<title>omikuji_test</title>
 </head>
 <body>
-<p>あなたの今日の運勢は<b><?php echo $result; ?></b>です。</p>
+<p><?php echo $result; ?></p>
 </body>
 </html>
