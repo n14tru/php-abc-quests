0462d48c66ce6a4a038a34d65a7cbc5e85e748e8
n14.tru <n14.tru@gmail.com>
2015-01-30 20:57:44 +0900

    おみじくプログラム　応用編（解答例）

--- practices/03/omikuji/index.php
+++ practices/03/omikuji/index.php
@@ -3,20 +3,26 @@ switch (rand(0,9)) {
 	case 0;
 	case 1;
 	case 2;
-		$result = 'ごめんなさい。あなたの今日の運勢は<b>大凶</b>です。';
+		$result = '大凶';
 		break;
 	case 3;
 	case 4;
 	case 5;
 	case 6;
 	case 7;
-		$result = 'あなたの今日の運勢は<b>吉</b>です。';
+		$result = '吉';
 		break;
 	case 8;
 	case 9;
-		$result = 'おめでとうございます！あなたの今日の運勢は<b>大吉</b>です。';
+		$result = '大吉';
 		break;
 }
+$templates = array(
+	'大凶' => 'ごめんなさい。あなたの今日の運勢は<b>大凶</b>です。',
+	'吉' => 'あなたの今日の運勢は<b>吉</b>です。',
+	'大吉' => 'おめでとうございます！あなたの今日の運勢は<b>大吉</b>です。',
+	);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -25,6 +31,6 @@ switch (rand(0,9)) {
 	<title>omikuji_test</title>
 </head>
 <body>
-<p><?php echo $result; ?></p>
+<p><?php echo $templates[$result]; ?></p>
 </body>
 </html>
